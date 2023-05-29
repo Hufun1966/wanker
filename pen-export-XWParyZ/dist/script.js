ba06b83cb37c3ecb5fc2be3d40a595c52c4098f2
Hufun1966 <130964730+Hufun1966@users.noreply.github.com>
2023-05-29 14:49:13 +0800

    Update script.js

--- pen-export-XWParyZ/dist/script.js
+++ pen-export-XWParyZ/dist/script.js
@@ -1,1 +1,8 @@
 document.write("大家好，我是胡程峯 ")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
